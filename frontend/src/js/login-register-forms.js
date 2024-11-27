cb23388ccf46fba8bebf7c290b94955b8637009b
barrera77 <30675869+barrera77@users.noreply.github.com>
2024-11-27 00:59:24 -0700

    Added Login Function

--- frontend/src/js/login-register-forms.js
+++ frontend/src/js/login-register-forms.js
@@ -1,5 +1,8 @@
-//Get elements from the DOM
+import { getData } from "../utils/api-utility.js";
 
+const MEMBERS_ENDPOINT = "/members";
+
+//Get elements from the DOM
 const toggleFormSignIn = document.querySelector("#goto-signin");
 const toggleFormRegister = document.querySelector("#goto-register");
 const registrationForm = document.querySelector("#customer-registration-form");
@@ -12,12 +15,12 @@ const registrationFormInputs = document.querySelectorAll(
 const loginFormInputs = document.querySelectorAll(
   "#customer-signin-form input"
 );
-
 const invalidFeedbackMessages = document.querySelectorAll(".invalid-feedback");
-
 const btnLogout = document.querySelector(".btn-logout");
 
 let isLogged = false;
+
+let membersList = [];
 
 document.addEventListener("DOMContentLoaded", () => {
   onInit();
@@ -40,12 +43,22 @@ function onInit() {
     logOut();
   }
 
+  //Toggle forms
   if (toggleFormSignIn) {
     toggleFormSignIn.addEventListener("click", toggleSignInForm);
   }
 
   if (toggleFormRegister) {
     toggleFormRegister.addEventListener("click", toggleRegisterForm);
+  }
+
+  //Handle form submissions
+  if (registrationForm) {
+    registrationForm.addEventListener("submit", handleRegistrationForm);
+  }
+
+  if (signinForm) {
+    signinForm.addEventListener("submit", handleSigninForm);
   }
 }
 
@@ -73,6 +86,51 @@ function toggleSignInForm(event) {
 }
 
 /**
+ * handle login form submission
+ * @param {*} event
+ */
+function handleSigninForm(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+
+  //get data from the form
+  const loginData = Object.fromEntries(formData.entries());
+  console.log("data", loginData);
+
+  //Validate data
+  const isValidForm = validateLoginForm(loginData);
+
+  //if is valid data log in
+  if (isValidForm) {
+    let isSuccess = memberLogin(loginData);
+
+    if (isSuccess) {
+      isLogged = true;
+      resetForms();
+      hideForms();
+      logOut();
+    } else {
+      isLogged = false;
+    }
+  }
+}
+
+/**
+ * validate the login form data
+ * @param {*} data
+ * @returns
+ */
+function validateLoginForm(data) {
+  let isValid = true;
+
+  isValid = validateField(data.username, "invalid-login-username") && isValid;
+  isValid = validateField(data.password, "invalid-login-password") && isValid;
+
+  return Boolean(isValid);
+}
+
+/**
  * toggle to the register form if the customer does not have an account
  * @param {*} event
  */
@@ -97,6 +155,177 @@ function toggleRegisterForm(event) {
 }
 
 /**
+ * handle registration form submission
+ * @param {*} event
+ */
+function handleRegistrationForm(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+
+  const customerData = Object.fromEntries(formData.entries());
+  console.log("customer data", customerData);
+
+  const isValidForm = validateRegistrationForm(customerData);
+  const isValidPhone = validatePhone(customerData.phone);
+  console.log(isValidPhone);
+
+  if (isValidForm) {
+    console.log("is the form valid?", isValidForm);
+    if (!isValidPhone) {
+      document.querySelector("#invalid-phone").classList.add("d-block");
+    } else if (isValidPhone) {
+      document.querySelector("#invalid-phone").classList.remove("d-block");
+
+      const isSuccess = registerNewCustomers(
+        customerData.name,
+        customerData.address,
+        customerData.city,
+        customerData.email,
+        customerData.phone,
+        customerData.username,
+        customerData.password
+      );
+
+      if (isSuccess) {
+        alert(
+          "Registration succesful. Sign in for a better shopping experience"
+        );
+        resetForms();
+      } else {
+        alert("Customer already exists!!!");
+      }
+    }
+  }
+}
+
+/**
+ * validate the forms
+ */
+function validateRegistrationForm(data) {
+  let isValid = true;
+
+  isValid = validateField(data.name, "invalid-name") && isValid;
+  isValid = validateField(data.address, "invalid-address") && isValid;
+  isValid = validateField(data.city !== "0", "invalid-city") && isValid;
+  isValid = validateField(data.email, "invalid-email") && isValid;
+  isValid = validateField(data.username, "invalid-username") && isValid;
+  isValid = validateField(data.password, "invalid-password") && isValid;
+
+  return Boolean(isValid);
+}
+
+function registerNewCustomers(
+  name,
+  address,
+  city,
+  email,
+  phone,
+  username,
+  password
+) {}
+
+/**
+ * verify customer and login
+ * @param {*} data
+ */
+async function memberLogin(data) {
+  let loggedCustomer = JSON.parse(localStorage.getItem("logedCustomer")) || "";
+  let localCustomers = JSON.parse(localStorage.getItem("customers")) || [];
+
+  //Get the customers data
+  membersList = await getData(MEMBERS_ENDPOINT);
+
+  // Check if the customer already exists in the data
+  const existingCustomer = membersList.find(
+    (customer) =>
+      customer.username === data.username && customer.password === data.password
+  );
+
+  // Check if the customer already exists in the local storage
+  const existingLocalCustomer = localCustomers.find(
+    (customer) =>
+      customer.username === data.username && customer.password === data.password
+  );
+
+  //validate  customer
+  if (existingCustomer) {
+    let verifiedCustomer =
+      loggedCustomer.username === existingCustomer.username;
+
+    if (verifiedCustomer) {
+      document.querySelector(".user-name-label").textContent =
+        verifiedCustomer.name;
+
+      if (btnLogout) {
+        btnLogout.classList.remove("d-none");
+      }
+    } else {
+      localStorage.setItem("logedCustomer", JSON.stringify(existingCustomer));
+      document.querySelector(".user-name-label").textContent =
+        existingCustomer.name;
+
+      if (btnLogout) {
+        btnLogout.classList.remove("d-none");
+      }
+      resetForms();
+      return true;
+    }
+  } else if (existingLocalCustomer) {
+    let verifiedCustomer =
+      loggedCustomer.username === existingLocalCustomer.username;
+
+    if (verifiedCustomer) {
+      document.querySelector(".user-name-label").textContent =
+        verifiedCustomer.name;
+
+      if (btnLogout) {
+        btnLogout.classList.remove("d-none");
+      }
+    } else {
+      localStorage.setItem(
+        "logedCustomer",
+        JSON.stringify(existingLocalCustomer)
+      );
+      document.querySelector(".user-name-label").textContent =
+        existingLocalCustomer.name;
+
+      if (btnLogout) {
+        btnLogout.classList.remove("d-none");
+      }
+      resetForms();
+      return true;
+    }
+  } else {
+    console.log(
+      "The username and password you've entered doesn't match any account. Please Try again"
+    );
+    alert(
+      "The username and password you've entered doesn't match any account. Please Try again"
+    );
+    return false;
+  }
+}
+
+/**
+ * if user logs out, show the forms
+ */
+function logOut() {
+  btnLogout.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    localStorage.removeItem("logedCustomer");
+    document.querySelector(".user-name-label").textContent = "";
+
+    if (formsContainer) {
+      formsContainer.classList.remove("d-none");
+    }
+
+    btnLogout.classList.add("d-none");
+  });
+}
+
+/**
  * reset the forms
  */
 function resetForms() {
@@ -118,4 +347,69 @@ function resetFeedbaackMessages() {
   invalidFeedbackMessages.forEach((message) => {
     message.classList.remove("d-block");
   });
+}
+
+/**
+ * Validate phone number format
+ * @param {*} phone
+ * @returns
+ */
+function validatePhone(phone) {
+  const regex = /^\d{3}-\d{3}-\d{4}$/;
+  return regex.test(phone);
+}
+
+/**
+ * validate individual fields
+ * @param {*} value
+ * @param {*} elementId
+ */
+function validateField(value, elementId) {
+  const messageElement = document.getElementById(elementId);
+
+  if (!value) {
+    messageElement.classList.add("d-block");
+    return false;
+  } else {
+    messageElement.classList.remove("d-block");
+    return true;
+  }
+}
+
+/**
+ * Add event listeners to registration inputs to handle feedback messages
+ */
+
+registrationFormInputs.forEach((input) => {
+  input.addEventListener("input", (event) => {
+    const inputId = input.id;
+    const messageElement = document.getElementById(`invalid-${inputId}`);
+
+    if (input.value) {
+      messageElement.classList.remove("d-block");
+    }
+  });
+});
+
+/**
+ * Add event listeners to login inputs to handle feedback messages
+ */
+loginFormInputs.forEach((input) => {
+  input.addEventListener("input", (event) => {
+    const inputId = input.id;
+    const messageElement = document.getElementById(`invalid-login-${inputId}`);
+
+    if (input.value) {
+      messageElement.classList.remove("d-block");
+    }
+  });
+});
+
+//if user is logged in, hide the forms
+function hideForms() {
+  if (isLogged) {
+    if (formsContainer) {
+      formsContainer.classList.add("d-none");
+    }
+  }
 }
